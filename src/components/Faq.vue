922316144f5ecfedef4de08744d8d9ef3b6dc7c3
sajibcse68 <sajibcse68@gmail.com>
2018-02-06 17:37:30 +0600

    Add a CSS framework

---
+++ src/components/Faq.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="faq">
+    FAQ
+  </div>
+</template>
+
+/* eslint-disable */
+
+<script>
+export default {
+  name: 'faq'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass" scoped>
+@import '../mq'
+
+</style>
