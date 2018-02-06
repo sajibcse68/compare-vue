a5a1d219da34da0df266cfdc1938d00f5bf2a486
sajibcse68 <sajibcse68@gmail.com>
2018-02-06 22:54:02 +0600

    Add hero sections (banner)

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="home">
-    Home
+    <section class="hero">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">{{ heading }}</h1>
+          <div class="is-two-thirds column is-paddingless">
+            <h2 class="subtitle is-4">{{ subheading }}</h2>
+          </div>
+          <a class="button is-large is-primary" id="learn">Learn more</a>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'home'
+  name: 'home',
+  data () {
+    return {
+      heading: 'Soaring to new heights',
+      subheading: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    }
+  }
 }
 </script>
 
@@ -14,4 +30,19 @@ export default {
 <style lang="sass" scoped>
 @import '../mq'
 
+.hero
+  background: url('../assets/clouds.jpg')
+  background-size: cover
+
+  .title
+    +mobile
+      font-weight: bold
+    +tablet
+      font-size: 2.5rem
+    +desktop
+      font-size: 4rem
+      margin-top: 2rem
+
+h2
+  margin: 1.5rem 0 2rem 0 !important
 </style>
