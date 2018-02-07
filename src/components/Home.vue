fb751cf55438383806e2b3d8df1188994e9e106c
sajibcse68 <sajibcse68@gmail.com>
2018-02-07 09:54:27 +0600

    Add supporting content (card, body columns text, etc.)

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,6 +11,49 @@
         </div>
       </div>
     </section>
+
+    <section class="section">
+      <div class="container">
+        <div class="columns pd is-desktop">
+          <div class="column is-1 has-text-centered">
+            <i class="fa fa-cog is-primary"></i>
+          </div>
+          <div class="column is-one-third-desktop">
+            <p class="title"><strong>We provide superior logistics so that your business can succeed in a crazy world.</strong></p>
+          </div>
+          <div class="column">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</strong></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="columns pd">
+        <div class="column">
+          <div class="card">
+            <div class="card-content">
+              <p class="title">"I think it's an absolutely excellent tool for our business. I can't survive without this thing."</p>
+              <p class="subtitle">- Gary Simon</p>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <div class="card-content">
+              <p class="title">"I think it's an absolutely excellent tool for our business. I can't survive without this thing."</p>
+              <p class="subtitle">- Gary Simon</p>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <div class="card-content">
+              <p class="title">"I think it's an absolutely excellent tool for our business. I can't survive without this thing."</p>
+              <p class="subtitle">- Gary Simon</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -45,4 +88,15 @@ export default {
 
 h2
   margin: 1.5rem 0 2rem 0 !important
+
+.fa-cog
+  font-size: 40px
+
+#learn
+  +desktop
+    margin-bottom: 2rem
+
+.pd
+  +tablet
+    padding: 2em 0
 </style>
