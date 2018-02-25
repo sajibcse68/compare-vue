19ceeb5ba2c96658ebabddc4234d2254656a80e3
sajib <sajib@appscode.com>
2018-02-25 23:19:59 +0600

    Allow user to create new Quote

---
+++ src/components/WonderfulQuotes/QuoteGrid.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="row">
+
+    <single-quote v-for="quote in quotes" :key="quote.id">aa{{ quote }}</single-quote>
+
+  </div>
+</template>
+
+<script>
+import SingleQuote from './QuoteSingle';
+
+export default {
+  props: ['quotes'],
+  components: {
+    SingleQuote: SingleQuote
+  }
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/WonderfulQuotes/Quotes.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <h1>Quotes View</h1>
+    <ac-quote-gride :quotes="quotes"></ac-quote-gride>
+  </div>
+</template>
+
+<script>
+import QuoteGrid from './QuoteGrid';
+export default {
+  data: () => {
+    return {
+      quotes: [
+        'Just a Quote to see something'
+      ],
+      maxQuotes: 10
+    };
+  },
+  components: {
+    AcQuoteGride: QuoteGrid
+  }
+};
+</script>
