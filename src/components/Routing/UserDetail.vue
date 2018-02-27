2a98c354b2ea6965027ef02a3bb9ce2a30eece97
sajib <sajib@appscode.com>
2018-02-27 21:33:07 +0600

    Pass the hash fragment to router

--- src/components/Routing/UserDetail.vue
+++ src/components/Routing/UserDetail.vue
@@ -3,8 +3,28 @@
     <h3>Some User Details</h3>
     <p>User Loaded has ID: {{ $route.params.id }}</p>
     <router-link
-    tag="button"
-    :to="{name: 'UserEdit', params: { id: $route.params.id }, query: { locale: 'en', q: '100' } }"
-    class="btn btn-primary">Edit User</router-link>
+      tag="button"
+      :to="{ name: 'UserEdit', params: { id: this.$route.params.id }, query: { locale: 'en',q: '100'}, hash: '#data' }"
+      class="btn btn-primary">Edit User</router-link>
   </div>
 </template>
+
+<script>
+  export default {
+    data: () => {
+      return {
+        btnLink: {
+          name: 'UserEdit',
+          params: {
+            id: this.$route.params.id
+          },
+          query: {
+            locale: 'en',
+            q: '100'
+          },
+          hash: '#data'
+        }
+      }
+    }
+  }
+</script>
--- src/components/Routing/UserEdit.vue
+++ src/components/Routing/UserEdit.vue
@@ -3,5 +3,8 @@
     <h1>User Edit Page</h1>
     <p>Locale: {{ $route.query.locale }} </p>
     <p>Analytics: {{ $route.query.q }} </p>
+
+    <div style="height: 700px"></div>
+    <p id="data">Some extra Data</p>
   </div>
 </template>
