9f94aac1ca57ba8f7a84a3a25ca86ea0f3d8d547
sajib <sajib@appscode.com>
2018-02-28 14:07:45 +0600

    Use centralized store (state management)

---
+++ src/components/Vuex/Counter.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <button class="btn btn-primary" @click="increment">Increment</button>
+    <button class="btn btn-primary" @click="decrement">Decrement</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    increment() {
+      console.log('counter+: ', this.$store.state.counter);
+      this.$store.state.counter++;
+    },
+    decrement() {
+      console.log('counter-: ', this.$store.state.counter);
+      this.$store.state.counter--;
+    }
+  }
+}
+</script>
---
+++ src/components/Vuex/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+    <h1>Vuex</h1>
+    <app-result></app-result>
+    <app-another-result></app-another-result>
+    <hr>
+    <app-counter ></app-counter>
+  </div>
+</template>
+
+<script>
+import Counter from './Counter.vue';
+import Result from './Result.vue';
+import AnotherResult from './AnotherResult.vue';
+export default {
+  components: {
+    appCounter: Counter,
+    appResult: Result,
+    appAnotherResult: AnotherResult,
+  }
+}
+</script>
