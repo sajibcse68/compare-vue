8fab7b5aece1ff2d2d2537f5367f20931ba19766
sajib <sajib@appscode.com>
2018-03-01 16:35:24 +0600

    Create Stock components

--- src/components/stockRider/stocks/Stocks.vue
+++ src/components/stockRider/stocks/Stocks.vue
@@ -1,5 +1,32 @@
 <template>
-  <div>
-    <h1>The Stock Component</h1>
+  <div class="row">
+
+    <app-stock v-for="stock in stocks" :key="stock.id"></app-stock>
+   
   </div>
 </template>
+
+<script>
+import Stock from './Stock';
+export default {
+  data () {
+    return {
+      stocks: [
+        { id: 1, name: 'BMW', price: 10 },
+        { id: 2, name: 'Google', price: 20 },
+        { id: 3, name: 'Apple', price: 100 },
+        { id: 4, name: 'Twiller', price: 30 }
+      ]
+    }
+  },
+  components: {
+    appStock: Stock
+  }
+}
+</script>
+
+<style scoped>
+div.row {
+  margin-top: 30px;
+}
+</style>
