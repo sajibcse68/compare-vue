d489f14d48b15eae184b78fd82a26f17e085928f
sajib <sajib@appscode.com>
2018-03-02 11:26:05 +0600

    Connect the Vuex with portfolio

--- src/components/stockRider/portfolio/Portfolio.vue
+++ src/components/stockRider/portfolio/Portfolio.vue
@@ -1,5 +1,25 @@
 <template>
   <div>
     <h1>The Portfolio Component</h1>
+    <app-stock v-for="stock in stocks" :key="stock.id" :stock="stock"></app-stock>
   </div>
 </template>
+
+<script>
+import { mapGetters } from 'vuex';
+import Stock from './Stock';
+export default {
+  computed: {
+    ...mapGetters ({
+      stocks: 'stockPortfolio'
+    })
+  },
+  components: {
+    appStock: Stock
+  }
+}
+</script>
+
+<style>
+
+</style>
