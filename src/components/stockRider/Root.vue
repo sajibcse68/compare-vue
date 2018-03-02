7fdae65044d7111d0e21db1ce1efc4e8a896cf51
sajib <sajib@appscode.com>
2018-03-02 12:57:40 +0600

    Animate the Routes using Transition CSS

--- src/components/stockRider/Root.vue
+++ src/components/stockRider/Root.vue
@@ -5,7 +5,11 @@
 
     <div class="row">
       <div class="col-md-12">
-        <router-view></router-view>
+        <transition name="slide" mode="out-in">
+
+          <router-view></router-view>
+
+        </transition>
       </div>
     </div>
 
@@ -26,5 +30,32 @@ export default {
 <style>
 body {
   padding: 30px;
+}
+
+.slide-enter-active {
+  animation: slide-in 200ms ease-out forwards;
+}
+.slide-leave-active {
+  animation: slide-out 200mx ease-out forwards;
+}
+@keyframes slide-in {
+  from {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
+@keyframes slide-out {
+  from {
+    transform: translateY(-);
+    opacity: 1;
+  }
+  to {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
 }
 </style>
